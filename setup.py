1c8a6a646e12e811e893ef953955be5067d06e2c
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-07-30 08:39:10 +0300

    chore: add copyright

--- setup.py
+++ setup.py
@@ -1,3 +1,33 @@
+"""
+Copyright (C) 2023 Maxim Slipenko.
+
+This file is part of the python-LimeReport library.
+
+The python-LimeReport library is free software; you can redistribute it and/or modify
+it under the terms of either:
+
+  * the GNU Lesser General Public License as published by the Free
+    Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+
+or
+
+  * the GNU General Public License as published by the Free Software
+    Foundation; either version 3 of the License, or (at your option) any
+    later version.
+
+or both in parallel, as here.
+
+The python-LimeReport library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received copies of the GNU General Public License and the
+GNU Lesser General Public License along with the python-LimeReport library. If not,
+see https://www.gnu.org/licenses/. 
+"""
+
 import os
 import re
 import subprocess
@@ -7,7 +37,6 @@ import platform
 
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
-from wheel.bdist_wheel import bdist_wheel
 
 
 USE_ZINT_ENV_KEY = 'LIMEREPORT_USE_ZINT'
