ad9c71946db34f95cf93d73926c89b93762a5489
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-08-19 16:59:32 +0300

    try fix

--- cibuildwheel/before_build.sh
+++ cibuildwheel/before_build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+PYTHON_VERSION=$(python3 -c "import platform; print('.'.join(list(platform.python_version_tuple())[:-1]))")
 PYTHON_PLATFORM=$(python3 -c "from packaging.tags import sys_tags; print(next(sys_tags()).platform.lower().replace(\"-\", \"_\").replace(\".\", \"_\").replace(\" \", \"_\"))")
 
+echo "Python version: $PYTHON_VERSION"
 echo "Python platform: $PYTHON_PLATFORM"
 
-curl -L https://github.com/python-LimeReport/pyside6/releases/download/py${PYTHON_VERSION}-qt${QT_VERSION}/extra-$PYTHON_PLATFORM.tar.gz | tar -h -zx -C /usr/local
+curl -sL https://github.com/python-LimeReport/pyside6/releases/download/py${PYTHON_VERSION}-qt${QT_VERSION}/extra-$PYTHON_PLATFORM.tar.gz | tar -h -zx -C /usr/local
