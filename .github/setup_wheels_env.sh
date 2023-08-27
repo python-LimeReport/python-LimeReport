4f64ea838c80a4e57e01a7f3160142d5fb8528ad
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-08-27 12:35:40 +0300

    refactor

---
+++ .github/setup_wheels_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+AUDITWHEEL_REPAIR_OPTIONS="$(cat $GITHUB_WORKSPACE/cibuildwheel/exclude_list.txt | sed -e "s/.*/--exclude &/" | xargs)"
+echo "AUDITWHEEL_REPAIR_OPTIONS=\"$AUDITWHEEL_REPAIR_OPTIONS\"" >> $GITHUB_ENV
--- cibuildwheel/auditwheel_repair_options.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-AUDITWHEEL_REPAIR_OPTIONS="$(cat $SCRIPT_DIR/exclude_list.txt | sed -e "s/.*/--exclude &/" | xargs)"
-
-echo "AUDITWHEEL_REPAIR_OPTIONS=\"$AUDITWHEEL_REPAIR_OPTIONS\""
