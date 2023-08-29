4517bf087ecca7c8fe67ff7ac08b7a879b5171c1
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-08-29 10:17:37 +0300

    refactor

--- cibuildwheel/before_build.sh
+++ cibuildwheel/before_build.sh
@@ -6,4 +6,4 @@ PYTHON_PLATFORM=$(python3 -c "from packaging.tags import sys_tags; print(next(sy
 echo "Python version: $PYTHON_VERSION"
 echo "Python platform: $PYTHON_PLATFORM"
 
-curl -sL https://github.com/python-LimeReport/pyside6/releases/download/py${PYTHON_VERSION}-qt${QT_VERSION}/extra-$PYTHON_PLATFORM.tar.gz | tar -h -zx -C /usr/local
+curl -sL https://github.com/python-LimeReport/pyside/releases/download/py${PYTHON_VERSION}-qt${QT_VERSION}/extra-$PYTHON_PLATFORM.tar.gz | tar -h -zx -C /usr/local
