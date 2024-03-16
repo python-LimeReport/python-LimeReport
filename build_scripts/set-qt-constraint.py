d7f225d5a197a1ce886bccba2aa481870dd605bc
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-03-16 20:39:34 +0300

    fix

--- build_scripts/set-qt-constraint.py
+++ build_scripts/set-qt-constraint.py
@@ -17,5 +17,17 @@ def main():
 	with open(p, 'w', encoding='utf-8') as f:
 		f.write(d)
 
+	with open('cibuildwheel/exclude_list.txt', encoding='utf-8') as f:
+		d = f.read()
+	
+	# get major and minor from minver
+	major, minor = args.minver.split('.')
+
+	d = d.replace('libshiboken6.abi3.so.6.4', f'"libshiboken6.abi.so.{major}.{minor}"')
+	d = d.replace('libpyside6.abi3.so.6.4', f'"libpyside6.abi.so.{major}.{minor}"')
+	
+	with open('cibuildwheel/exclude_list.txt', 'w', encoding='utf-8') as f:
+		f.write(d)
+
 if __name__ == '__main__':
 	main()
