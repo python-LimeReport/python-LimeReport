494baa79e7333b6459e162bc9132b4e656291a3b
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-03-16 23:40:11 +0300

    wip

--- .github/repair_wheel.sh
+++ .github/repair_wheel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SITE_PACKAGES=$(python3 -c "import sysconfig; print(sysconfig.get_paths()['purelib'])")
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SITE_PACKAGES/PySide6:$SITE_PACKAGES/shiboken6 auditwheel -v repair $AUDITWHEEL_REPAIR_OPTIONS -w {dest_dir} {wheel}
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SITE_PACKAGES/PySide6:$SITE_PACKAGES/shiboken6 auditwheel -v repair $AUDITWHEEL_REPAIR_OPTIONS -w $1 $2
