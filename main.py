26f0b98649256d83aff0132f07d45ed6389247c3
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-03-16 17:16:29 +0300

    remove extra files

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-import LimeReport
-
-
-if __name__ == "__main__":
-    app = QApplication([])
-    report = LimeReport.ReportEngine()
-    sys.exit(report.designReport())
