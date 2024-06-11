362f4616f841acbdeafda3824bcb0eff222de61c
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-06-11 10:30:56 +0300

    debug

--- .github/install_deps.sh
+++ .github/install_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if [ "$(uname -m)" = "aarch64" ]; then
-    apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y libxslt1.1
+    ldd -r $(which shiboken6)
+    # apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y libxslt1.1
 fi
