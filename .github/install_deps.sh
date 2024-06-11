02faa09247a7ac37e6f3e386173b6dd5ae30afa9
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-06-11 10:49:49 +0300

    fix

--- .github/install_deps.sh
+++ .github/install_deps.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 if [ "$(uname -m)" = "aarch64" ]; then
-    ldd -r $(which shiboken6)
-    # apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y libxslt1.1
+    apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y libxslt1.1 libllvm11
 fi
