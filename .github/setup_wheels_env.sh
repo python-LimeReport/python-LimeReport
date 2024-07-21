8e6d13e7882cb393c88acdb01d92fe7692a54c4b
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-07-21 13:05:52 +0300

    try fix build

--- .github/setup_wheels_env.sh
+++ .github/setup_wheels_env.sh
@@ -2,3 +2,17 @@
 
 AUDITWHEEL_REPAIR_OPTIONS="$(cat $GITHUB_WORKSPACE/cibuildwheel/exclude_list.txt | sed -e "s/.*/--exclude &/" | xargs)"
 echo "AUDITWHEEL_REPAIR_OPTIONS=$AUDITWHEEL_REPAIR_OPTIONS" >> "$GITHUB_ENV"
+
+LIBS_TO_EXCLUDE=(
+    "libshiboken6.abi3.so.$QT_MAJOR_MINOR_VERSION"
+    "libpyside6.abi3.so.$QT_MAJOR_MINOR_VERSION"
+    "libpyside6qml.abi3.so.$QT_MAJOR_MINOR_VERSION"
+)
+
+for lib in "${LIBS_TO_EXCLUDE[@]}"; do
+    BASE_AUDITWHEEL_REPAIR_OPTIONS+=" --exclude $lib"
+done
+
+sed -i "s/shiboken6==.*/shiboken6==$QT_VERSION/" $GITHUB_WORKSPACE/pyproject.toml
+sed -i "s/PySide6==.*/PySide6==$QT_VERSION/" $GITHUB_WORKSPACE/pyproject.toml
+sed -i "s/shiboken6_generator==.*/shiboken6_generator==$QT_VERSION/" $GITHUB_WORKSPACE/pyproject.toml
